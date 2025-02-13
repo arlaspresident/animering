d9bfe449c4160bdb1b55601136528ef28ee59a81
arlaspresident <jankovic4901@gmail.com>
2025-02-13 17:23:10 +0100

    lagt till hjärtan istället för ballonger

---
+++ dist/animation.ea41651d.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const button = document.querySelector(".heart-button");
+    const container = document.querySelector(".heart-container");
+    button.addEventListener("click", ()=>{
+        for(let i = 0; i < 5; i++){
+            let heart = document.createElement("div");
+            heart.classList.add("heart");
+            container.appendChild(heart);
+            setTimeout(()=>heart.remove(), 5000);
+        }
+    });
+});
+
+//# sourceMappingURL=animation.ea41651d.js.map
